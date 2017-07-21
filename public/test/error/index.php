b7a309765c52b4cfe96e877e7ba25950679e10c8
pavliksedoy <pavliksedoy@gmail.com>
2017-07-21 10:52:58 +0300

    21.07.17 (1)

--- public/test/error/index.php
+++ public/test/error/index.php
@@ -16,12 +16,30 @@ class ErrorHandler
             error_reporting(0);
         }
         set_error_handler([$this, 'errorHandler']);
+        ob_start();
+        register_shutdown_function([$this, 'fatalErrorHandler']);
     }
 
     public function errorHandler($errno, $errstr, $errfile, $errline)
     {
         $this->displayError($errno, $errstr, $errfile, $errline);
         return true;
+    }
+
+    public function fatalErrorHandler()
+    {
+        $error = error_get_last();
+        if (!empty($error) && $error['type'] & (E_ERROR | E_PARSE | E_COMPILE_ERROR | E_CORE_ERROR) ) {
+            ob_end_clean();
+            $this->displayError($error['type'], $error['message'], $error['file'], $error['line']);
+        } else {
+            ob_end_flush();
+        }
+    }
+
+    public function exceptionHandler(Exception $e)
+    {
+        $this->displayError('Исключение', $e->getMessage(), $e->getFile(), $e->getLine(), $e->getCode());
     }
 
     protected function displayError($errno, $errstr, $errfile, $errline, $response = 500)
@@ -38,4 +56,15 @@ class ErrorHandler
 
 new ErrorHandler();
 
-echo $test;
+//echo $test;
+//test();
+
+/*try {
+    if (empty($test)) {
+        throw new Exception('Упс, исключение');
+    }
+} catch (Exception $e) {
+    var_dump($e);
+}*/
+
+throw new Exception('Упс, исключение');
