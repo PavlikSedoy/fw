097c94edee31376883da677050f49db71c0a62f8
pavliksedoy <pavliksedoy@gmail.com>
2017-10-01 21:48:28 +0300

    Widgets - menu (final part)

---
+++ public/menu/my_menu.php
@@ -0,0 +1,8 @@
+<li class="test">
+    <a href="?id=<?=$id;?>"><?=$category['title'];?></a>
+    <?php if (isset($category['child'])): ?>
+        <ul>
+            <?= $this->getMenuHtml($category['child']);?>
+        </ul>
+    <?php endif; ?>
+</li>
